c88cdc52757aab3a26c518a68511d25d4467db69
Patrik Karlström <patrik@trixon.se>
2026-01-06 10:21:01 +0100

    Add pom

---
+++ src/main/java/se/trixon/quakeml4j/Quakeml4j.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package se.trixon.quakeml4j;
+
+/**
+ *
+ * @author Patrik Karlström <patrik@trixon.se>
+ */
+public class Quakeml4j {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
